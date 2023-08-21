f4a8922b93828c5b421c966dc32f71607ab25ef0
murilo-marian <murilomarian.mm@gmail.com>
2023-08-21 18:36:03 -0300

    adicionar método de salvar por xml

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -4,6 +4,7 @@ import contatos.Contato;
 import contatos.Contatos;
 import persistencia.IPersistencia;
 import persistencia.PersistenciaJSON;
+import persistencia.PersistenciaXML;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,10 +13,11 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
         IPersistencia teste = new PersistenciaJSON();
+        IPersistencia xml = new PersistenciaXML();
         Contato contato = new Contato("Murilo", LocalDate.of(2003, 9, 12),
                 "48988032892", "murilomarian.mm@gmail.com", teste);
         Contato contato2 = new Contato("Saloi", LocalDate.of(2003, 1, 12),
-                "55555555555", "saloi@gmail.com", teste);
+                "55555555555", "saloi@gmail.com", xml);
         contato.salvar();
         contato2.salvar();
         Contatos agenda = new Contatos(teste);
--- src/main/java/persistencia/PersistenciaXML.java
+++ src/main/java/persistencia/PersistenciaXML.java
@@ -1,13 +1,78 @@
 package persistencia;
 
 import contatos.Contato;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class PersistenciaXML implements IPersistencia{
     @Override
     public void salvar(Contato contato) {
+        File data = new File("data.xml");
 
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder docBuilder = domFactory.newDocumentBuilder();
+            Document doc;
+            Element rootElement;
+
+            if (data.exists() && data.length() > 40) {
+                doc = docBuilder.parse(data);
+                rootElement = doc.getDocumentElement();
+            } else {
+                doc = docBuilder.newDocument();
+                rootElement = doc.createElement("contatos");
+                doc.appendChild(rootElement);
+            }
+
+            Element contatoElement = doc.createElement("contato");
+
+            contatoElement.appendChild(criarElemento(doc, "name" , contato.getNome()));
+            contatoElement.appendChild(criarElemento(doc, "nascimento"  , String.valueOf(contato.getNascimento())));
+            contatoElement.appendChild(criarElemento(doc, "email", contato.getEmail()));
+            contatoElement.appendChild(criarElemento(doc, "telefone"  , contato.getTelefone()));
+            
+            rootElement.appendChild(contatoElement);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            transformer.setOutputProperty(OutputKeys.INDENT, "no");
+            DOMSource source = new DOMSource(doc);
+
+            StreamResult result = new StreamResult(data);
+            transformer.transform(source, result);
+
+
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private Element criarElemento(Document doc, String nome, String valor) {
+        Element node = doc.createElement(nome);
+        node.appendChild(doc.createTextNode(valor));
+        return node;
     }
 
     @Override
